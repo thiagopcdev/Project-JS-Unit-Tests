84a8086c247fc0bba396eab71d74be0bd60b0cd9
Thiago Prado <thiagopc_dev@hotmail.com>
2022-04-27 16:43:10 -0300

    Projeto de ES6 e testes unitarios

---
+++ README.md
@@ -0,0 +1,21 @@
+## Projeto de ES6 e Testes Unitários!
+
+Implementei várias funções para atender aos requisitos propostos e/ou testes unitários para garantir que as implementações das funções estão corretas.
+
+### Requisitos do projeto
+
+#### 1 - Implemente a função `average`
+
+#### 2 - Implemente os casos de teste para a função `numbers`
+
+#### 3 - Implemente a função `vqv`
+
+#### 4 - Implemente os casos de teste para a função `circle`
+
+#### 5 - Implemente a função `createStudent`
+
+#### 6 - Implemente os casos de teste para a função `productDetails`
+
+#### 8 - Implemente a função `myCounter`
+
+#### 9 - Implemente os casos de teste e a função `createMenu`
---
+++ tests/productDetails.spec.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    //assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Arroz', 'Feijao').length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Arroz', 'Feijao'), 'object');
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(Object.values(productDetails('Arroz', 'Feijao')[0]), Object.values(productDetails('Arroz', 'Feijao')[1]));
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    const funcao = productDetails('Arroz', 'Feijao');
+    assert.strictEqual(funcao[0].details.productId.replace(funcao[0].name, ''), '123');
+    assert.strictEqual(funcao[1].details.productId.replace(funcao[1].name, ''), '123');
+  });
+});
